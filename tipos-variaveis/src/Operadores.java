23098139a94d51cb89bffc79261def58b4f43344
Ian <iancunha20@gmail.com>
2024-11-11 00:41:44 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,45 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        /*
+         * String nome = "Ian Cunha";
+         * int idade = 24;
+         * double peso = 60.5;
+         * char sexo = 'M';
+         * boolean doadorOrgao = true;
+         * Date dataNascimento = new Date();
+         */
+
+        // Concatenacao
+        // String linguagem = "LINGUAGEM " + "JAVA";
+        // System.out.println(linguagem);
+
+        /*
+         * int numero = 5;
+         * numero = - numero;
+         * System.out.println(numero);
+         * numero = numero * -1;
+         * System.out.println(numero);
+         */
+
+        /*
+         * 
+         * int numero = 5;
+         * 
+         * // x repeticao
+         * 
+         * // numero = numero +1;
+         * 
+         * System.out.println(numero++);
+         * 
+         * System.out.println(numero);
+         */
+
+
+         boolean variavel = true;
+
+         variavel = !variavel;
+
+         System.out.println(variavel);
+    }
+}
